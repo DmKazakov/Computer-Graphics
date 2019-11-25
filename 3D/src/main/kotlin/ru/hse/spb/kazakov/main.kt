69339fecd5112caa11701196cc97ade12fe87171
Dmitry <qudeed@gmail.com>
2019-11-25 09:11:35 +0300

    hw2

---
+++ 3D/src/main/kotlin/ru/hse/spb/kazakov/main.kt
@@ -0,0 +1,10 @@
+package ru.hse.spb.kazakov
+
+private const val MODEL_FILE = "ivysaur.obj"
+const val PHONG_FRAGMENT_SHADER = "phong_fragment.glsl"
+const val PHONG_VERTEX_SHADER = "phong_vertex.glsl"
+const val TEXTURE = "perish.png"
+
+fun main() {
+    Frame(MODEL_FILE)
+}
